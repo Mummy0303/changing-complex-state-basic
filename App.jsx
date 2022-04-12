52b45d1aa993c3dcc815c6d2a7e3e485da0cf74c
Mertcan Kara <97619078+Mummy0303@users.noreply.github.com>
2022-04-12 21:26:33 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+
+function App() {
+  const [isMousedOver, setColor] = useState(false);
+
+  function handleMouseOver() {
+    setColor(true);
+  }
+  function handleMouseOut() {
+    setColor(false);
+  }
+
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: ""
+  });
+
+  function handleChange(event) {
+    const { value, name } = event.target;
+
+    setContact((prevValue) => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: prevValue.lName
+        };
+      } else if (name === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: value
+        };
+      } else if (name === "email") {
+        return {
+          fName: prevValue.fName,
+          lName: prevValue.lName,
+          email: value
+        };
+      }
+    });
+  }
+
+  return (
+    <div className="container">
+      <h1>
+        Hello. {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form>
+        <input onChange={handleChange} name="fName" placeholder="First Name" />
+        <input onChange={handleChange} name="lName" placeholder="Last Name" />
+        <input onChange={handleChange} name="email" placeholder="Email" />
+        <button
+          style={{ backgroundColor: isMousedOver ? "black" : "white" }}
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
